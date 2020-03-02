0a9697fc86610d12025a501754e2513284ff9d30
Daniel Nava Viveros <danielnava901@hotmail.com>
2020-03-02 12:45:03 -0600

    Updates

---
+++ precache-manifest.23f1e247f1ee4afe28a63c46e16dab01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91088a31a0b12c4d261a27eb14092d0",
+    "url": "/dnv-react-component/index.html"
+  },
+  {
+    "revision": "a005c0547f5694ebdaa3",
+    "url": "/dnv-react-component/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "e034d2e6e01ca37e5e86",
+    "url": "/dnv-react-component/static/js/2.4136e49d.chunk.js"
+  },
+  {
+    "revision": "a005c0547f5694ebdaa3",
+    "url": "/dnv-react-component/static/js/main.d6c5c628.chunk.js"
+  },
+  {
+    "revision": "9e9a2572236953843efd",
+    "url": "/dnv-react-component/static/js/runtime-main.3f7691e7.js"
+  }
+]);
