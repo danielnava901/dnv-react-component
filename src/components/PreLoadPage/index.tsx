db63ff2c6e8365f116c9e95211ffb63d16803f8e
Daniel Nava Viveros <danielnava901@hotmail.com>
2020-03-05 18:25:38 -0600

    comit

--- src/components/PreLoadPage/index.tsx
+++ src/components/PreLoadPage/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 
 interface PreLoadPageInterface {
+  text?: string
 };
 
 const PreLoadPage : React.FC<PreLoadPageInterface> = (props) => {
     return <div className="row">
-        <div className="col-sm-12">Cargando.......</div>
+        <div className="col-sm-12">Cargando.......{props.text}</div>
     </div>
 };
 
